a6e40f7bf7e74433f47250676053748f2eea4584
AsharSiddique <ashar4404293@cloud.neduet.edu.pk>
2025-06-19 00:13:31 +0500

    Added notes app with CRUD API

--- elevate/settings.py
+++ elevate/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'accounts',
+    'notes',
     'corsheaders'
 ]
 
--- elevate/urls.py
+++ elevate/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from notes.views import NoteViewSet
+
+router = DefaultRouter()
+router.register(r'notes', NoteViewSet, basename='note')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('', include(router.urls)),  # Directly mount the notes API
 ]
-
